7bbfde0d7e08163ded9b2c7f1303422442cfb44a
james demarco <james.dem77co@gmail.com>
2025-02-17 17:08:06 -0500

    updating example on annotations

--- Annotations/CustomAnnotation2.java
+++ Annotations/CustomAnnotation2.java
@@ -1,5 +1,10 @@
 package Annotations;
 
 public @interface CustomAnnotation2 {
+
+    // value allows you to assign an annotation value without declaring a name for
+    // the value
+    // It only works if the value is the only element in the annotation
+    // If there are multiple elements, you must declare a name for each element
     int value();
 }
